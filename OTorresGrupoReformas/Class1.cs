89c1d82b93645ad5e48f49b01d04184f76a74ee7
KDanielRosas <k.daniel.rosas99@gmail.com>
2023-10-13 15:38:54 -0600

    Add project files.

---
+++ OTorresGrupoReformas/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Class1
+    {
+
+        public static int Factorial()
+        {
+            Console.Write("Por favor digite un número entero positivo: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nEl Factorial de " + num + " es: " + fact);
+            return fact;
+        }
+
+        public static int Matriz()
+        {
+            int[,,] students1 = new int[2, 3, 4];
+
+            //Matriz 1
+            students1[0, 0, 0] = 1;
+            students1[0, 0, 1] = 2;
+            students1[0, 0, 2] = 3;
+
+            students1[0, 1, 0] = 4;
+            students1[0, 1, 1] = 5;
+            students1[0, 1, 2] = 6;
+
+            students1[0, 2, 0] = 7;
+            students1[0, 2, 1] = 8;
+            students1[0, 2, 2] = 9;
+
+            int[,,] students = new int[2, 3, 4];
+
+            //Matriz 2
+            students[1, 0, 0] = 1;
+            students[1, 0, 1] = 1;
+            students[1, 0, 2] = 1;
+
+            students[1, 1, 0] = 5;
+            students[1, 1, 1] = 6;
+            students[1, 1, 2] = 1;
+
+            students[1, 2, 0] = 7;
+            students[1, 2, 1] = 8;
+            students[1, 2, 2] = 1;
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int z = 0; z < 3; z++)
+                    {
+
+                        int resultado = students1[x, y, z] + students[x, y, z];
+                        Console.Write($"{resultado}");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            return 0;
+        }
+
+        public static int[,] SumarMatrices()
+        {
+
+            int[,] matriz1 =
+            { {1, 2, 3},{4, 5, 6}, {7, 8, 9} };
+
+            int[,] matriz2 =
+            { {1, 1, 1},{5, 6, 1},{7, 8, 1} };
+
+            int filasMatriz1 = matriz1.GetLength(0);
+            int filasMatriz2 = matriz1.GetLength(1);
+
+            int[,] resultado = new int[filasMatriz1, filasMatriz2];
+
+            for (int i = 0; i < filasMatriz1; i++)
+            {
+                for (int j = 0; j < filasMatriz2; j++)
+                {
+                    resultado[i, j] = matriz1[i, j] + matriz2[i, j];
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ PL/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bienvenido Usuario");
+
+            int opcion;
+            Console.WriteLine("\n ¿Qué deseas realizar?");
+            Console.WriteLine("\n 1. Factorial");
+            Console.WriteLine("\n 2. Matriz");
+            //Console.WriteLine("\n 3. Actualizar");
+            //Console.WriteLine("\n 4. Eliminar");
+            //Console.WriteLine("\n 5. Carga masiva .txt");
+            //Console.WriteLine("\n 6. Carga masiva .Excel");
+
+            opcion = int.Parse(Console.ReadLine());
+            //int x;
+            switch (opcion)
+            {
+                case 1:
+
+                    //PL.Usuario.GetAll();
+
+                    BL.Class1.Factorial();
+
+                    break;
+
+                case 2:
+
+                    Console.Write("Las matrices son las siguientes: \n\n");
+                    BL.Class1.Matriz();
+
+                    //int[,] matriz1 = 
+                    //{
+                    //    {1, 2, 3},
+                    //    {4, 5, 6},
+                    //    {7, 8, 9}
+                    //};
+
+                    //int[,] matriz2 = 
+                    //{
+                    //    {1, 1, 1},
+                    //    {5, 6, 1},
+                    //    {7, 8, 1}
+                    //};
+
+                    int[,] resultado = BL.Class1.SumarMatrices( );
+
+                    Console.WriteLine("Resultado de la suma de las matricez es:");
+
+                    for (int i = 0; i < resultado.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < resultado.GetLength(1); j++)
+                        {
+                            Console.Write(resultado[i, j] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+
+                //case 3:
+
+                //    break;
+
+                default:
+                    Console.WriteLine("\n No es una opcion validad");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
