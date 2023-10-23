78a1a881ed64293c5e042c3646d673b95edb649d
KDanielRosas <k.daniel.rosas99@gmail.com>
2023-10-23 17:00:57 -0600

    Operaciones basicos

--- OTorresGrupoReformas/Class1.cs
+++ OTorresGrupoReformas/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -97,5 +98,194 @@ namespace BL
             }
             return resultado;
         }
+
+        public static void Calculadora()
+        {
+            while (true)
+            {
+                Console.WriteLine("Calculadora básica");
+                Console.WriteLine("\n 1. Suma");
+                Console.WriteLine("\n 2. Resta");
+                Console.WriteLine("\n 3. Multiplicación");
+                Console.WriteLine("\n 4. División");
+                Console.WriteLine("\n 5. Salir");
+
+
+                int opcion;
+                opcion = int.Parse(Console.ReadLine());
+
+                Console.Write("Por favor digite un número entero positivo: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Por favor digite un segundo número entero positivo: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                double resultado = 0;
+
+                switch (opcion)
+                {
+                    case 1: // Suma
+                        resultado = num1 + num2;
+                        break;
+                    case 2: // Resta
+                        resultado = num1 - num2;
+                        break;
+                    case 3: // Multiplicación
+                        resultado = num1 * num2;
+                        break;
+                    case 4: // División
+                        if (num2 != 0)
+                        {
+                            resultado = num1 / num2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir por cero.");
+                            continue;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\n No es una opcion validad");
+                        break;
+                }
+
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+        }
+
+        public static void Piramide()
+        {
+
+            Console.Write("Por favor digite un número entero positivo: ");
+            int nivel = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nivel; i++)
+            {
+                // Imprimir espacios en blanco (alineación a la derecha)
+                for (int j = 1; j <= nivel - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Imprimir asteriscos
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(); // Salto de línea para la siguiente fila
+            }
+        }
+
+        public static void TablasMultiplicar()
+        {
+            Console.Write("Ingresa la tabla de multiplicar que quieres tener :");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tablas de multiplicar del :" + numero);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+
+                Console.WriteLine(numero + "x" + i + "=" + resultado);
+            }
+        }
+
+        public static void NumeroParesNones()
+        {
+            Console.WriteLine("Elija qué desea mostrar:");
+
+            Console.WriteLine("1. Primeros 100 números pares ");
+
+            Console.WriteLine("2. Primeros 100 números impares");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                Console.WriteLine("Primeros 100 números pares:");
+                for (int i = 2; i <= 100 * 2; i += 2)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Primeros 100 números impares:");
+
+                for (int i = 1; i <= 100 * 2; i += 2)
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+
+        public static void FormulaGeneral()
+        {
+            Console.WriteLine("Resolución de una ecuación de segundo grado  con la formula geral (-b +-raiz b^2 -4ac)/2a");
+
+            Console.Write("Ingrese el coeficiente A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el coeficiente B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el coeficiente C: ");
+            int c = int.Parse(Console.ReadLine());
+
+            //Hacer primero la raiz que esta dentro 
+            double resultado = b * b - 4 * a * c;
+
+            if (resultado > 0)
+            {
+                // Math.Sqrt es para sacar la raiz
+                double x1 = (-b + Math.Sqrt(resultado)) / (2 * a);
+
+                double x2 = (-b - Math.Sqrt(resultado)) / (2 * a);
+                Console.WriteLine("Las solucion de x1 = " + x1 + " y de x2 = " + x2);
+            }
+
+        }
+
+        public static void TablaTienda()
+        {
+
+            // Crear un nuevo DataTable con dos columnas
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("TiendaID", typeof(int));
+            dataTable.Columns.Add("Tienda", typeof(string));
+
+            int IdTienda = 1; // Iniciar con el ID en 1
+
+            Console.WriteLine("Ingrese el nombre de la tienda o escribe la palabra 'exit' para salir :");
+
+            while (true)
+            {
+                Console.Write("Nombre de la tienda: ");
+                string nombre = Console.ReadLine();
+
+                if (nombre.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                DataRow row = dataTable.NewRow();
+                row["TiendaID"] = IdTienda;
+                row["Tienda"] = nombre;
+                dataTable.Rows.Add(row);
+                IdTienda++; // Incrementar el ID 
+            }
+
+            Console.WriteLine("Tabla de Tiendas:");
+            Console.WriteLine("ID   Nombre");
+            DataRow[] tiendas = dataTable.Select();
+            foreach (var tienda in tiendas)
+            {
+                //El numero -4 es valor de los espacion en cadenas 
+                Console.WriteLine($"{tienda["TiendaID"],-4} {tienda["Tienda"]}");
+            }
+        }
+
     }
 }
--- PL/Program.cs
+++ PL/Program.cs
@@ -17,18 +17,18 @@ namespace PL
             Console.WriteLine("\n ¿Qué deseas realizar?");
             Console.WriteLine("\n 1. Factorial");
             Console.WriteLine("\n 2. Matriz");
-            //Console.WriteLine("\n 3. Actualizar");
-            //Console.WriteLine("\n 4. Eliminar");
-            //Console.WriteLine("\n 5. Carga masiva .txt");
-            //Console.WriteLine("\n 6. Carga masiva .Excel");
+            Console.WriteLine("\n 3. Operaciones Basicas");
+            Console.WriteLine("\n 4. Piramide de astediscos");
+            Console.WriteLine("\n 5. Tabla de multiplicación");
+            Console.WriteLine("\n 6. Numero pares o impares");
+            Console.WriteLine("\n 7. Formula general");
+            Console.WriteLine("\n 8. Tabla de tiendas");
 
             opcion = int.Parse(Console.ReadLine());
             //int x;
             switch (opcion)
             {
                 case 1:
-
-                    //PL.Usuario.GetAll();
 
                     BL.Class1.Factorial();
 
@@ -39,21 +39,21 @@ namespace PL
                     Console.Write("Las matrices son las siguientes: \n\n");
                     BL.Class1.Matriz();
 
-                    //int[,] matriz1 = 
-                    //{
-                    //    {1, 2, 3},
-                    //    {4, 5, 6},
-                    //    {7, 8, 9}
-                    //};
+                    /* int[,] matriz1 =
+                     {
+                         {1, 2, 3},
+                         {4, 5, 6},
+                         {7, 8, 9}
+                     };
 
-                    //int[,] matriz2 = 
-                    //{
-                    //    {1, 1, 1},
-                    //    {5, 6, 1},
-                    //    {7, 8, 1}
-                    //};
+                     int[,] matriz2 =
+                     {
+                         {1, 1, 1},
+                         {5, 6, 1},
+                         {7, 8, 1}
+                     };*/
 
-                    int[,] resultado = BL.Class1.SumarMatrices( );
+                    int[,] resultado = BL.Class1.SumarMatrices();
 
                     Console.WriteLine("Resultado de la suma de las matricez es:");
 
@@ -67,9 +67,39 @@ namespace PL
                     }
                     break;
 
-                //case 3:
+                case 3:
 
-                //    break;
+                    BL.Class1.Calculadora();
+
+                    break;
+
+                case 4:
+
+                    BL.Class1.Piramide();
+
+                    break;
+
+                case 5:
+
+                    BL.Class1.TablasMultiplicar();
+
+                    break;
+                case 6:
+
+                    BL.Class1.NumeroParesNones();
+
+                    break;
+
+                case 7:
+
+                    BL.Class1.FormulaGeneral();
+
+                    break;
+
+                case 8:
+                    BL.Class1.TablaTienda();
+
+                    break;
 
                 default:
                     Console.WriteLine("\n No es una opcion validad");
